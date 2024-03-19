003a3161177f7a99a9e11f5d6bbcbe10321184be
andreisc <andrei.castro1991@gmail.com>
2024-03-19 19:11:06 -0300

    Fianlizando aulas de componentes

--- Projects/projectComponents/src/app/card/card.component.ts
+++ Projects/projectComponents/src/app/card/card.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',  
   styleUrl: './card.component.scss'
+  /*
+  , encapsulation: ViewEncapsulation.None 
+  Serve para deixar o css deste componente global. Resumindo não serve pra nada já que se for usar css global, 
+  utilize a classe styles.scss
+  
+  , encapsulation: ViewEncapsulation.ShadowDom   
+  O css definido nesse component tem prioridade max sobre ele, msm se tiver estilos diferentes na classe global styles.scss, 
+  prevalece o que está aqui*/
 })
 export class CardComponent {
     
--- Projects/projectComponents/src/app/cards/cards.module.ts
+++ Projects/projectComponents/src/app/cards/cards.module.ts
@@ -6,6 +6,7 @@ import { CardRoxoComponent } from '../card-roxo/card-roxo.component';
 import { CardButtonComponent } from '../card-button/card-button.component';
 import { CardRoxoButtonComponent } from '../card-roxo-button/card-roxo-button.component';
 import { CardCancelButtonComponent } from '../card-cancel-button/card-cancel-button.component';
+import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { CardCancelButtonComponent } from '../card-cancel-button/card-cancel-but
     CardCancelButtonComponent
   ],  
   imports: [
-    CommonModule
+    CommonModule,
+    MatSliderModule
   ],
   exports: [
     //Sempre que to need utilizar component de um modulo, deve importar o Modulo todo.
