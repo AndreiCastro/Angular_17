185c5ac1c6fd13f2e4bac651a52e0199b8571385
andreisc <andrei.castro1991@gmail.com>
2024-06-04 18:37:57 -0300

    Arrumando projetos

--- Projects/ProjectProjecaoConteudo/projectProjecaoSeletores/src/app/components/card/directives/card-text-title.directive.ts
+++ Projects/ProjectProjecaoConteudo/projectProjecaoSeletores/src/app/components/card/directives/card-text-title.directive.ts
@@ -1,8 +1,10 @@
 import { Directive } from '@angular/core';
 
 @Directive({
-  selector: 'app-card-text-title',
-  host: {'class': 'ca-u-card-text-title'}
+  //selector: 'app-card-text-title',
+  // pode-se criar diretivas com multiplos Seletores, só add no selector mais 1, e no html chamar a tag q irá utiizar, ex:
+  selector: 'app-card-text-title, [appCardTextTitle]',
+  host: {'class': 'ca-u-card-text-title'} //Aqui onde vc referencia o css, no style.scss tem essa classe 
 })
 export class CardTextTitleDirective {
 }
